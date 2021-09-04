848b8b5dd2a35c4b624602712143222c26ab7d67
D-Perez85 <damianperez22@hotmail.com>
2021-09-04 04:25:42 -0300

    Metodo para servicio del verbo POST

--- src/app/modules/productos/services/productos.service.ts
+++ src/app/modules/productos/services/productos.service.ts
@@ -2,13 +2,17 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 
 @Injectable()
-export class ProductoService{
-    private productoUrl = 'http://localhost:3002/api/modules/productos';//VA HASTA LA RUTA DEL MODULO, SE CONCANTENA EL RESTO
-    
-    constructor(private http: HttpClient){}
+export class ProductoService {
+  private productoUrl = "http://localhost:3002/api/modules/productos"; //VA HASTA LA RUTA DEL MODULO, SE CONCANTENA EL RESTO
 
-    getProductos(){
-        return this.http.get(this.productoUrl + '/productos')
-    }
+  constructor(private http: HttpClient) {}
+
+  guardarProducto(producto: any) {
+    return this.http.post(this.productoUrl + "/producto", producto);
+  }
+
+  getProductos() {
+    return this.http.get(this.productoUrl + "/productos");
+  }
+
 }
-
