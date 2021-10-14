bc7065670b30e04c9eef923c6e50f904a1b2f92a
Luis Parada <luisparadawagner@gmail.com>
2021-10-14 12:07:36 -0300

    Se agrega socket

--- src/app/modules/productos/consulta-productos/consulta-producto.component.ts
+++ src/app/modules/productos/consulta-productos/consulta-producto.component.ts
@@ -1,23 +1,38 @@
 import { Component, OnInit, OnDestroy } from "@angular/core";
 import { ProductoService } from "../services/productos.service";
+
+import { io } from "socket.io-client";
+import { environment } from "./../../../../environments/environment";
+
 @Component({
-  selector: "app-consulta-producto",
-  templateUrl: "consulta-producto.component.html",
-  styleUrls: ["consulta-producto.component.css"],
+    selector: "app-consulta-producto",
+    templateUrl: "consulta-producto.component.html",
+    styleUrls: ["consulta-producto.component.css"],
 })
-
 export class ConsultarProductoComponent implements OnInit, OnDestroy {
-  constructor(private usuarioService: ProductoService) {}
+    private socket;
+    productos: any[] = [];
 
-  ngOnInit() {
-    this.getProductos();
-  }
-  getProductos() {
-    this.usuarioService.getProductos().subscribe(data => {
-      //debugger;
-    })
-  }
+    constructor(private usuarioService: ProductoService) {
+        this.socket = io(environment.WS).on("connect", () => {
+            console.log("Socket conectado en angular");
+        });
 
-  ngOnDestroy() {}
-  
+        this.socket.on("crearProducto", (producto: any) => {});
+
+        this.socket.emit("borrarProducto", "Producto Borrado");
+    }
+
+    ngOnInit() {
+        this.getProductos();
+    }
+    getProductos() {
+        this.usuarioService.getProductos().subscribe((data: any) => {            
+            if (data.status === "success") {
+                this.productos = data.data;
+            }
+        });
+    }
+
+    ngOnDestroy() {}
 }
